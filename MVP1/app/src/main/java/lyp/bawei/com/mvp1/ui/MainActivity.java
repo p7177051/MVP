fa9da2f791f42f46e7c08c76fe34bf9f063aeb6d
Administrator <705710297@qq.com>
2017-05-09 09:51:39 +0800

    liyanpeng66666

--- MVP1/app/src/main/java/lyp/bawei/com/mvp1/ui/MainActivity.java
+++ MVP1/app/src/main/java/lyp/bawei/com/mvp1/ui/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements Iview{
         MainPre mainPre = new MainPre(this);
         mainPre.getData();
     }
-//赵倩是猪 我喜欢猪
+//赵倩是猪 老母猪
     @Override
     public void showData(MainModelBean mainModelBean) {
         List<MainModelBean.OthersBean> others = mainModelBean.others;
